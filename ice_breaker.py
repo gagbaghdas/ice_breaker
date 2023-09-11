1c5c470da175c9ab342a8f3802677a9dac43dae4
Gagik Baghdasaryan <gag@Gagiks-MacBook-Pro.local>
2023-09-11 18:44:04 +0400

    added fullstack app

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
@@ -10,7 +11,7 @@ from third_parties.twitter import scrape_user_tweets
 from output_parsers import person_intel_parser, PersonIntel
 
 
-def ice_break(name: str) -> PersonIntel:
+def ice_break(name: str) -> Tuple[PersonIntel, str]:
     linkedin_profie_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkeding_profile(linkedin_profile_url=linkedin_profie_url)
 
@@ -41,7 +42,7 @@ def ice_break(name: str) -> PersonIntel:
 
     result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
     print(result)
-    return person_intel_parser.parse(result)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
 
 
 name = "Elon Musk"
