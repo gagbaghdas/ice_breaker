56caf96dc4f08e33fa48f94cfbadbffb724ab1fd
Gagik Baghdasaryan <gag@Gagiks-MacBook-Pro.local>
2023-09-12 10:57:41 +0400

    removed twitter part

--- ice_breaker.py
+++ ice_breaker.py
@@ -15,13 +15,13 @@ def ice_break(name: str) -> Tuple[PersonIntel, str]:
     linkedin_profie_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkeding_profile(linkedin_profile_url=linkedin_profie_url)
 
-    twitter_username = twitter_lookup_agent(name=name)
-    print(twitter_username)
+    # twitter_username = twitter_lookup_agent(name=name)
+    # print(twitter_username)
 
-    tweets = scrape_user_tweets(username=twitter_username, num_tweets=100)
+    # tweets = scrape_user_tweets(username=twitter_username, num_tweets=100)
 
     summary_template = """
-        given the Linkedin information {information} and twitter {twitter_information} about person from I wnat you to create:
+        given the Linkedin information {information} about person from I want you to create:
         1. a short summary
         2. two interesting facts about them
         3. A topic that may interest them
@@ -29,7 +29,7 @@ def ice_break(name: str) -> Tuple[PersonIntel, str]:
         \n{format_instructions}
     """
     summary_promt_template = PromptTemplate(
-        input_variables=["linkedin_information", "twitter_information"],
+        input_variables=["information"],
         template=summary_template,
         partial_variables={
             "format_instructions": person_intel_parser.get_format_instructions()
@@ -40,7 +40,7 @@ def ice_break(name: str) -> Tuple[PersonIntel, str]:
 
     chain = LLMChain(llm=llm, prompt=summary_promt_template)
 
-    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    result = chain.run(information=linkedin_data)
     print(result)
     return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
 
